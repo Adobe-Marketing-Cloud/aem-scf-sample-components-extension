e532f918248cb39e51691a1d3b3516259c80bd38
Siddharth Palaniswami <palanisw@adobe.com>
2014-08-13 01:06:57 -0700

    Add JS code for handling a setStatus operation
    Add a view object for Ideas that extends the OOTB SCF.TopicView.
    Add a setStatus function that handles the set status operation. This
    view function retrieves the status to be set, and asks the model to
    communicate with the server to make the update.
    Add a Model object for an Idea that extends the OOTB SCF.Topic model.
    Add a setStatus function that accepts a status and posts to the
    endpoint for setting the status. When a response is received from the
    endpoint the model updates itself and triggers an event.
    The view automatically re-renders itself on the client side when the
    model update event is fired.

--- content/jcr_root/apps/acme/components/ideation/forum/clientlibs/forum.js
+++ content/jcr_root/apps/acme/components/ideation/forum/clientlibs/forum.js
@@ -15,7 +15,53 @@
  */
 (function($CQ, _, Backbone, SCF) {
     "use strict";
+     var Idea = SCF.Topic.extend({
+         modelName: "IdeaModel",
+         setStatus: function(status) {
+             var error = _.bind(function(jqxhr, text, error) {
+                this.log.error("error setting status " + error);
+                this.trigger('idea:statuserror', {
+                    'error': error
+                });
+            }, this);
+            var success = _.bind(function(response) {
+                this.set(response.response);
+                this.trigger('idea:statusset', {
+                    model: this
+                });
+                this.trigger(this.events.UPDATED, {
+                    model: this
+                });
+            }, this);
+            var postData = {
+                'status': status,
+                ':operation': 'social:setIdeaStatus'
+            };
+            $CQ.ajax(SCF.config.urlRoot + this.get('id') + SCF.constants.URL_EXT, {
+                dataType: 'json',
+                type: 'POST',
+                xhrFields: {
+                    withCredentials: true
+                },
+                data: this.addEncoding(postData),
+                'success': success,
+                'error': error
+            });
+         }
+     });
+
+	var IdeaView = SCF.TopicView.extend({
+        viewName: "Idea",
+        setStatus: function(e){
+         	var status = this.getField("status");
+         	this.model.setStatus(status);
+            e.preventDefault();
+     	}
+	});
+
+	SCF.Idea = Idea;       
+    SCF.IdeaView = IdeaView;
     SCF.registerComponent('acme/components/ideation/post', SCF.Post, SCF.PostView);
-    SCF.registerComponent('acme/components/ideation/topic', SCF.Topic, SCF.TopicView);
+    SCF.registerComponent('acme/components/ideation/topic', SCF.Idea, SCF.IdeaView);
     SCF.registerComponent('acme/components/ideation/forum', SCF.Forum, SCF.ForumView);
 })($CQ, _, Backbone, SCF);
